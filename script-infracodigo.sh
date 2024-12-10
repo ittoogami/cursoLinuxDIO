4291ac264061f718f0128dff240268d5cba7932a
Thiago Assunção <161776524+ittoogami@users.noreply.github.com>
2024-12-10 11:36:10 -0300

    Update script-infracodigo.sh

--- script-infracodigo.sh
+++ script-infracodigo.sh
@@ -18,7 +18,7 @@ echo 'Baixando e descompactando os arquivos...'
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
 
 echo 'Script Finalizado...'
